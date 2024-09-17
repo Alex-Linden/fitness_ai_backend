224945b1e5ef98eb4cdd0db60c051f7c300fd050
Alex Linden <alextl2004@gmail.com>
2024-09-17 12:08:04 -0700

    hello route

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get('/hello')
+def say_hello():
+    """Return simple "Hello" Greeting."""
+    html = "<html><body><h1>Hello</h1></body></html>"
+    return html
