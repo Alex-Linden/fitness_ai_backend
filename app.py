709f730a7d2a917158aa616659d0029b96e63cfa
Alex Linden <alextl2004@gmail.com>
2024-11-26 15:11:08 -0800

    add oauth config

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import jwt
 from flask import Flask, request, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -6,6 +8,62 @@ from models import db, connect_db, User
 
 
 app = Flask(__name__)
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+# From blog/tutorial TODO: register app with OAuth2Client and update secret keys
+app.config['OAUTH2_PROVIDERS'] = {
+    # Google OAuth 2.0 documentation:
+    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
+    'google': {
+        'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
+        'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://accounts.google.com/o/oauth2/token',
+        'userinfo': {
+            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+    },
+
+    # GitHub OAuth 2.0 documentation:
+    # https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+    'github': {
+        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
+    # TODO: validate that this is the right info for strava
+    # Strava OAuth 2.0 documentation:
+    # https://developers.strava.com/docs/authentication/
+    'strava': {
+        'client_id': os.environ.get('STRAVA_CLIENT_ID'),
+        'client_secret': os.environ.get('STRAVA_CLIENT_SECRET'),
+        'authorize_url': 'https://www.strava.com/oauth/authorize',
+        'token_url': 'https://www.strava.com/oauth/token',
+        'userinfo': {
+            'url': 'https://www.strava.com/api/v3/athlete',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['view_private'],
+    },
+}
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
 
 @app.get('/hello')
 def say_hello():
